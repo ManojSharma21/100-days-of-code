262eec08649f6054b2914a4dc39456c82b7b0e76
ManojSharma21 <43639174+ManojSharma21@users.noreply.github.com>
2018-10-21 14:22:57 +0530

    Create pratice.py

---
+++ day13/pratice.py
@@ -0,0 +1,17 @@
+# basic operators
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+m=12
+n=4.0
+o="is the best place to learn and practice coding!"
+# Read and save an integer, double, and String to your variables.
+print(i+m)
+# Print the sum of both integer variables on a new line.
+print(d+n)
+# Print the sum of the double variables on a new line.
+print(s+o)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
