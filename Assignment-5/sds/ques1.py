85db2fa93749478feae6e0446de4a3526e540d40
ManojSharma21 <43639174+ManojSharma21@users.noreply.github.com>
2018-11-07 01:39:13 +0530

    Add files via upload

---
+++ Assignment-5/sds/ques1.py
@@ -0,0 +1,2 @@
+for i in range(1,11,1):
+    print ("5 x %d = %d"%(i,i*5))
---
+++ Assignment-5/sds/ques2.py
@@ -0,0 +1,3 @@
+n=int(input("Enter the number of which you want to print table:"))
+for i in range(1,11,1):
+    print ("%d x %d = %d"%(n,i,i*n))
